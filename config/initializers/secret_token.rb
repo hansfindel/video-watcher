8c17fcc5d0204e442bcc0b2a0e0b2a8c1a2604ce
Hans <hanstrax@gmail.com>
2013-09-15 23:43:14 -0400

    starting an app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VideosWatcher::Application.config.secret_key_base = '0f62f1caf7d50adddce79e92770c62be95fe429d849f0cb86b75c6dcea8bbed5ae3699ccc292f2f828c84bdfecdd88f84017ac0bed06effb6a2ee95f9a950bb8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VideosWatcher::Application.config.session_store :cookie_store, key: '_videos_watcher_session'
